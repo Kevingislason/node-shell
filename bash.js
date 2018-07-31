2ba76a62ac4baeae476e712e3e4b2f747a8e6783
abby wigdale <abby.wigdale@gmail.com>
2018-07-31 11:31:22 -0500

    first commit

--- bash.js
+++ bash.js
@@ -1,1 +1,10 @@
-a
+process.stdout.write('prompt >');
+const pwd = require('./pwd');
+
+process.stdin.on('data', (data) => {
+  const cmd = data.toString().trim();
+  if(!pwd(cmd)){
+    process.stdout.write('You typed: ' + cmd);
+    process.stdout.write('\nprompt > ');
+  }
+})
---
+++ pwd.js
@@ -0,0 +1,9 @@
+
+module.exports = function(cmd){
+  if (cmd === 'pwd'){
+    process.stdout.write(__dirname);
+    process.stdout.write('\nprompt > ');
+    return true;
+    }
+    return false;
+}
