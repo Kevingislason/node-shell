f05a06ace0eb6f4ffedb5cc858dca2819742b951
Kevin Gislason <kevingislason@gmail.com>
2018-07-31 12:08:42 -0500

    ls

--- bash.js
+++ bash.js
@@ -1,10 +1,11 @@
 process.stdout.write('prompt >');
 const pwd = require('./pwd');
+const ls = require('./ls');
 
-process.stdin.on('data', (data) => {
+process.stdin.on('data', data => {
   const cmd = data.toString().trim();
-  if(!pwd(cmd)){
+  if (!pwd(cmd) && !ls(cmd)) {
     process.stdout.write('You typed: ' + cmd);
     process.stdout.write('\nprompt > ');
   }
-})
+});
--- pwd.js
+++ pwd.js
@@ -1,9 +1,8 @@
-
-module.exports = function(cmd){
-  if (cmd === 'pwd'){
+module.exports = function(cmd) {
+  if (cmd === 'pwd') {
     process.stdout.write(__dirname);
     process.stdout.write('\nprompt > ');
     return true;
-    }
-    return false;
-}
+  }
+  return false;
+};
